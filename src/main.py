8419ba76e8eb5ea3110c97834aa071dc327bb4df
chenxiaochun <sjzchenxiaochun@gmail.com>
2026-01-02 10:52:51 +0800

    feat: auto format on save

--- src/main.py
+++ src/main.py
@@ -9,13 +9,14 @@ client = OpenAI(
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
+
 completion = client.chat.completions.create(
     model="qwen3-max",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "你是谁？"},
     ],
-    stream=True
+    stream=True,
 )
 for chunk in completion:
     print(chunk.choices[0].delta.content, end="", flush=True)
